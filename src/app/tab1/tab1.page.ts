6b57bee89e38b87b47201110cf7c2256a75dbb4e
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-10-16 10:50:00 -0300

    Adicionado alerta

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Atenção',
+      message: 'Deseja mesmo favoritar esse filme?',
+      buttons: ['Cancelar', 'Confirmar'],
+    });
+
+    await alert.present();
+  }
 }
