f391b3a5e4737fe7bed6f95af78d204db83b28f1
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-10-16 21:21:32 -0300

    Construção do serviço temporário

--- src/app/services/dados.service.ts
+++ src/app/services/dados.service.ts
@@ -5,5 +5,28 @@ import { Injectable } from '@angular/core';
 })
 export class DadosService {
 
+  private dados: any = [];
+
   constructor() { }
+
+  guardarDados(index: string, dados: any): boolean {
+    if (index) {
+      this.dados[index] = dados;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  pegarDados(index: string): any {
+    if (index) {
+      return this.dados[index];
+    } else {
+      return null;
+    }
+  }
+
+  deletarDados(index: string): boolean {
+    return delete this.dados[index];
+  }
 }
