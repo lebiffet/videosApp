ccd56a34d88774cca6dd0bc082315566e062128a
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-10-17 20:27:33 -0300

    Serviço de busca de filmes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DadosService } from './services/dados.service';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, DadosService],
   bootstrap: [AppComponent],
 })
